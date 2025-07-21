73c2f5372eb43b70469bf420f1ef705c0921868f
sam065819 <sam065819@outlook.com>
2025-07-21 11:09:51 +0200

    Initial commit and bot

---
+++ bot.py
@@ -0,0 +1,18 @@
+import dotenv
+import discord
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+bot = discord.Bot()
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name} - {bot.user.id}')
+    
+@bot.slash_command(name='cat', description='meows')
+async def cat(ctx):
+    await ctx.respond('meow')
+    
+
+bot.run(os.getenv('TOKEN'))
