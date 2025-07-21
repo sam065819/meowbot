450a5293f7791d63ca6c6c2c2f6b257a05d979d8
sam065819 <sam065819@outlook.com>
2025-07-21 17:55:13 +0200

    fun cog, moved /cat to it

--- bot.py
+++ bot.py
@@ -16,24 +16,6 @@ async def on_ready():
     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="cat videos"))
     
 @bot.slash_command(
-    name="cat",
-    description="sends a cute kitty",
-    integration_types={
-        discord.IntegrationType.guild_install,
-        discord.IntegrationType.user_install,
-    },
-)
-async def cat(ctx):
-    async with aiohttp.ClientSession() as session:
-        async with session.get("https://api.thecatapi.com/v1/images/search") as resp:
-            data = await resp.json()
-            image_url = data[0]["url"]
-    embed = discord.Embed(title="kitty", description="meow", color=discord.Color.nitro_pink())
-    embed.set_image(url=image_url)
-    await ctx.respond(embed=embed)
-    
-    
-@bot.slash_command(
     name="ping",
     description="pong",
     integration_types={
---
+++ cogs/fun.py
@@ -0,0 +1,31 @@
+import discord
+import aiohttp
+from discord.ext import commands
+
+class Fun(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        
+    fun = discord.SlashCommandGroup("fun", "all the fun commands :3")
+    
+    @fun.command(
+        name='cat',
+        description='sends a cute kitty',
+        integration_types={
+            discord.IntegrationType.guild_install,
+            discord.IntegrationType.user_install,
+        },
+    )
+    async def cat(ctx):
+        async with aiohttp.ClientSession() as session:
+            async with session.get("https://api.thecatapi.com/v1/images/search") as resp:
+                data = await resp.json()
+                image_url = data[0]["url"]
+        embed = discord.Embed(title="kitty", description="meow", color=discord.Color.nitro_pink())
+        embed.set_image(url=image_url)
+        await ctx.respond(embed=embed)
+    
+    
+    
+def setup(bot):
+    bot.add_cog(Fun(bot))
