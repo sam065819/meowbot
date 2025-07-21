766240e032ada3b408e10cd15a280a1c043d38ab
sam065819 <sam065819@outlook.com>
2025-07-21 11:23:19 +0200

    initial moderation cog

---
+++ cogs/moderation.py
@@ -0,0 +1,10 @@
+import discord
+from discord.ext import commands
+
+class Moderation(commands.Cog):
+    def __init__(self, bot) :
+        self.bot = bot
+        
+    @discord.slash_command()
+    async def kick(self, ctx, member: discord.Member, reason: str):
+        discord.Member.kick(member, reason)
