8219b8048b0063a5887935b51c68caec4b9c653c
sam065819 <sam065819@outlook.com>
2025-07-21 12:14:46 +0200

    added ban command

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -15,5 +15,11 @@ class Moderation(commands.Cog):
         await member.kick()
         await ctx.respond(f"{member} has been kicked from the server.")
         
+    @moderation.command()
+    @commands.has_permissions(ban_members=True)
+    async def ban(self, ctx, member: discord.Member, reason: str):
+        await member.ban()
+        await ctx.respond(f"{member} has been banned from the server.")
+        
 def setup(bot):
     bot.add_cog(Moderation(bot))
